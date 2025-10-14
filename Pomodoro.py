469d670338b9e9eec99806bc5ca06678aed20c1d
Lord8Bits <62944891+Lord8Bits@users.noreply.github.com>
2025-10-14 20:05:04 +0100

    Removed Pause to follow pomodoro principal

--- Pomodoro.py
+++ Pomodoro.py
@@ -13,16 +13,16 @@ class Pomodoro(CTk):
         self.geometry('800x650')
         self.title('Pomodoro Timer')
 
-        self.workTime: float = 2 # 25 min study time
+        self.workTime: float = 90 # 25 min study time
         self.breakTime: float = 5 # 5 min break time
-
-        self.isPaused: bool = False
+        
         self.isRunning: bool = False
         self.isReset: bool = False
 
         self.grid_system()
         self.timer_label()
         self.buttons()
+        self.stop_thread()
 
     def grid_system(self):
         self.grid_columnconfigure((0,1), weight=1)
@@ -42,24 +42,12 @@ class Pomodoro(CTk):
         
         self.buttonStart.grid(row=1, column=0, columnspan=2, padx=20, pady=(10, 4))
 
-        self.buttonPause = CTkButton(self, width=50, text="Pause",
-                                    corner_radius=32, command=self.pause_logic)
-        
-        self.buttonPause.grid(row=2, column=0, columnspan=2, padx=20, pady=(10, 4))
         
         self.buttonReset = CTkButton(self, width=50, text="Reset",
                                     corner_radius=32, command=self.reset_logic)
         
         self.buttonReset.grid(row=3, column=0, columnspan=2, padx=20, pady=(10, 4))
     
-    def pause_logic(self):
-        if self.isRunning:
-            if self.isPaused:
-                self.pause_event.set()
-                self.isPaused = False
-            else:
-                self.pause_event.clear()
-                self.isPaused = True
     
     
     def start_logic(self):
@@ -68,9 +56,9 @@ class Pomodoro(CTk):
             self.buttonStart.configure(state=DISABLED)
             # event variable serves for the pause logic:
             self.pause_event = threading.Event()
-            self.pause_event.set()
+            self.pause_event.set() # .set() = unpaused , .clear() = paused
             # Start the countdown thread:
-            counting_worker = threading.Thread(target=self._countdown, args=[self.workTime], daemon=True)
+            counting_worker = threading.Thread(target=self._countdown, args=[self.workTime], daemon=True) # Creates new thread
             counting_worker.start()
 
     def reset_logic(self):
@@ -78,12 +66,15 @@ class Pomodoro(CTk):
             self.isReset = True
         else:
             self.isReset = False
-
+        
+    def stop_thread(self):
+        # Resets the timer to the original value and continues the thread to stop it when isReset is True
+        if self.isReset:
+            self.TimeLabel.configure(text=time.strftime("%M:%S", time.gmtime(self.workTime)))
 
     def _countdown(self, start: float):
         # Work time logic:
         while start > 0 and not self.isReset:
-            self.pause_event.wait()
             time.sleep(0.5)
             start -= 0.5
             self.TimeLabel.configure(text=time.strftime("%M:%S", time.gmtime(start)))
@@ -92,7 +83,6 @@ class Pomodoro(CTk):
         if start <= 0:
             start = self.breakTime # 5 minutes break
             while start > 0 and not self.isReset:
-                self.pause_event.wait()
                 time.sleep(0.5)
                 start -= 0.5
                 self.TimeLabel.configure(text=time.strftime("%M:%S", time.gmtime(start)))
@@ -102,6 +92,7 @@ class Pomodoro(CTk):
         self.TimeLabel.configure(text=time.strftime("%M:%S", time.gmtime(self.workTime)))
         
         self.isRunning = False
+        self.isReset = False
             
             
 app = Pomodoro()
