74ea4eb7265a089ef682b47236c714432b3725c7
Lord8Bits <62944891+Lord8Bits@users.noreply.github.com>
2025-10-14 21:56:35 +0100

    created sounds folder in assests, added ringtone to pomodoro timer.

--- Pomodoro.py
+++ Pomodoro.py
@@ -2,6 +2,7 @@ from customtkinter import * # type: ignore
 import threading
 import time
 import playsound
+import random
 
 
 # TODO: add a ringtone when timer ends
@@ -14,11 +15,13 @@ class Pomodoro(CTk):
         self.geometry('800x650')
         self.title('Pomodoro Timer')
 
-        self.work_time: float = 6 # 25 min study time
-        self.break_time: float = 3 # 5 min break time
+        self.work_time: float = 10 # 25 min study time
+        self.break_time: float = 10 # 5 min break time
 
         self.is_running: bool = False
         self.is_reset: bool = False
+
+        self.sound: str = "assests/sounds/bell.mp3"
 
         self.grid_system()
         self.timer_label()
@@ -30,7 +33,7 @@ class Pomodoro(CTk):
         self.grid_rowconfigure((0,1), weight=1)
     
     def timer_label(self):
-        FontManager.load_font("assests/MRKMaston-Bold.ttf")
+        FontManager.load_font("assests/fonts/MRKMaston-Bold.ttf")
         MRK_font = CTkFont(family="MRK Maston Bold", weight='bold', size=80)
         self.time_label = CTkLabel(self, text=time.strftime("%M:%S", time.gmtime(self.work_time))
                                   , font=MRK_font)
@@ -48,18 +51,20 @@ class Pomodoro(CTk):
         
         self.button_reset.grid(row=2, column=0, columnspan=2, padx=20, pady=(10, 4))
     
-    
-    
+    def fun(self):
+        chosen_sound = random.choices(["assests/sounds/bell.mp3", "assests/sounds/tubular_bells.mp3"], weights=[0.9, 0.1], k=1)[0]
+        self.sound = chosen_sound
     def start_logic(self):
         if not self.is_running:
             self.is_running = True
             self.button_start.configure(state=DISABLED)
-            # event variable serves for the pause logic:
-            self.pause_event = threading.Event()
-            self.pause_event.set() # .set() = unpaused , .clear() = paused
             # Start the countdown thread:
-            counting_worker = threading.Thread(target=self._countdown, daemon=True) # Creates new thread
+            counting_worker = threading.Thread(target=self._countdown, daemon=True) # Creates new thread for counting
             counting_worker.start()
+
+    def play_sound(self):
+        sound_worker = threading.Thread(target=playsound.playsound, args=[self.sound], daemon=True) # Thread for playing sound
+        sound_worker.start()
 
     def reset_logic(self):
         if self.is_running:
@@ -80,13 +85,17 @@ class Pomodoro(CTk):
                 time.sleep(0.1)
                 start -= 0.1
                 self.time_label.configure(text=time.strftime("%M:%S", time.gmtime(start)))
-        
+            # Plays Notification Sound:
+            self.fun()
+            if not self.is_reset: self.play_sound() # Plays a ringtone but doesn't when reset button is pressed
             # Break time logic:
             start = self.break_time + .1 # 5 minutes break
             while start > 0.1 and not self.is_reset:
                 time.sleep(0.1)
                 start -= 0.1
                 self.time_label.configure(text=time.strftime("%M:%S", time.gmtime(start)))
+            if not self.is_reset: self.play_sound()
+
 
         # Reset the button state back to normal and running/reset states as well as the Timer to the initial time.
         self.button_start.configure(state=NORMAL)
